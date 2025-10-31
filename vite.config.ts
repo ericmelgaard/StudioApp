72c276a3a7604e6579e2fd0e1408ca1bbaa40d7c
ericmelgaard <93993616+ericmelgaard@users.noreply.github.com>
2025-10-31 07:45:46 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,8 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  define: {
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://0ec90b57d6e95fcbda19832f.supabase.co'),
+    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJib2x0IiwicmVmIjoiMGVjOTBiNTdkNmU5NWZjYmRhMTk4MzJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4ODE1NzQsImV4cCI6MTc1ODg4MTU3NH0.9I8-U0x86Ak8t2DGaIk0HfvTSLsAyzdnz-Nw00mMkKw'),
+  },
 });
