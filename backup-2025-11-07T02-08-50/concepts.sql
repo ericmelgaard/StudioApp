537d130b8e96d69cfbd5f26f6478335d9b61c5bf
ericmelgaard <93993616+ericmelgaard@users.noreply.github.com>
2025-11-06 20:09:17 -0600

    Added backup-data.js

---
+++ backup-2025-11-07T02-08-50/concepts.sql
@@ -0,0 +1,15 @@
+-- Backup of concepts table
+-- Generated: 2025-11-07T02:08:51.878Z
+-- Records: 11
+
+INSERT INTO concepts (id, name, privilege_level, parent_level, domain_level, group_type_string, parent_key, created_at) VALUES (54, 'Auntie Anne''s', 1, 1, 2, '', 3, '2025-11-06T22:28:16.829033+00:00');
+INSERT INTO concepts (id, name, privilege_level, parent_level, domain_level, group_type_string, parent_key, created_at) VALUES (17, 'Church''s Chicken', 1, 1, 2, '', 3, '2025-11-06T22:28:16.829033+00:00');
+INSERT INTO concepts (id, name, privilege_level, parent_level, domain_level, group_type_string, parent_key, created_at) VALUES (50, 'Cinnabon', 1, 1, 2, '', 3, '2025-11-06T22:28:16.829033+00:00');
+INSERT INTO concepts (id, name, privilege_level, parent_level, domain_level, group_type_string, parent_key, created_at) VALUES (199, 'Compass Group USA', 1, 1, 2, '', 3, '2025-11-06T22:28:16.829033+00:00');
+INSERT INTO concepts (id, name, privilege_level, parent_level, domain_level, group_type_string, parent_key, created_at) VALUES (266, 'Compass USA: Bon Appetit', 1, 1, 2, '', 3, '2025-11-06T22:28:16.829033+00:00');
+INSERT INTO concepts (id, name, privilege_level, parent_level, domain_level, group_type_string, parent_key, created_at) VALUES (268, 'Compass USA: Canteen', 1, 1, 2, '', 3, '2025-11-06T22:28:16.829033+00:00');
+INSERT INTO concepts (id, name, privilege_level, parent_level, domain_level, group_type_string, parent_key, created_at) VALUES (232, 'Compass USA: Chartwells', 1, 1, 2, '', 3, '2025-11-06T22:28:16.829033+00:00');
+INSERT INTO concepts (id, name, privilege_level, parent_level, domain_level, group_type_string, parent_key, created_at) VALUES (316, 'Compass USA: Eurest', 1, 1, 2, '', 3, '2025-11-06T22:28:16.829033+00:00');
+INSERT INTO concepts (id, name, privilege_level, parent_level, domain_level, group_type_string, parent_key, created_at) VALUES (209, 'WAND Demos', 1, 1, 2, '', 3, '2025-11-06T22:28:16.829033+00:00');
+INSERT INTO concepts (id, name, privilege_level, parent_level, domain_level, group_type_string, parent_key, created_at) VALUES (214, 'WAND Development', 1, 1, 2, '', 3, '2025-11-06T22:28:16.829033+00:00');
+INSERT INTO concepts (id, name, privilege_level, parent_level, domain_level, group_type_string, parent_key, created_at) VALUES (73, 'Wand Sales (Internal)', 1, 1, 2, '', 3, '2025-11-06T22:28:16.829033+00:00');
